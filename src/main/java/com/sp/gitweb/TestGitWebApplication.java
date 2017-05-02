7654e66ea3f161d4695cbbaa37691a8bdbb31b3e
qingbo.wu <wuqb2007@163.com>
2017-05-02 16:11:53 +0800

    Test Git by Eclipse. by qingbo.wu

---
+++ src/main/java/com/sp/gitweb/TestGitWebApplication.java
@@ -0,0 +1,13 @@
+package com.sp.gitweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitWebApplication.class, args);
+		System.out.println("starting...");
+	}
+}
