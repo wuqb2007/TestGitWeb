d4be152013b3b35778c10b15aca00214b60025dc
qingbo.wu <wuqb2007@163.com>
2017-05-02 16:14:42 +0800

    Test Git by Eclipse 2.
    By qingbo.wu

---
+++ src/test/java/com/sp/gitweb/TestGitWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sp.gitweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestGitWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
